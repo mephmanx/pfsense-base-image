cacd5bd60b528bad9d6edeb181c840b8ff44375b
chris <chris@lyonsgroup.family>
2022-08-04 16:31:17 -0400

    creating 2.6.0 branch

--- init-script.sh
+++ init-script.sh
@@ -2,4 +2,4 @@
 
 echo "Downloading pfSense Image"
 curl -L -o /root/pfSense-CE-memstick-ADI.img.gz https://atxfiles.netgate.com/mirror/downloads/pfSense-CE-memstick-ADI-"$1"-RELEASE-amd64.img.gz
-echo $(sha256sum /root/pfSense-CE-memstick-ADI.img.gz)
+sha256sum /root/pfSense-CE-memstick-ADI.img.gz
