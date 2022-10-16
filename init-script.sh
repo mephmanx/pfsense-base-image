a03510fc947e32f63cd6668275935f4501c7d1b0
clyons793 <christopher_lyons@comcast.com>
2022-10-16 10:07:39 -0400

    adding logging

--- init-script.sh
+++ init-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-echo "Downloading pfSense Image"
+echo "Downloading pfSense Image $1"
 curl -L -o /root/pfSense-CE-memstick-ADI.img.gz https://atxfiles.netgate.com/mirror/downloads/pfSense-CE-memstick-ADI-"$1"-RELEASE-amd64.img.gz
 sha256sum /root/pfSense-CE-memstick-ADI.img.gz
